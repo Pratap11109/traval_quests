214b027744486ffd75da4e9d2b09f2024e9a41e5
pratap-thakur11 <pratap.thakur@baffle.io>
2023-07-29 18:14:06 +0530

    added main page form

--- travel_app/travel_planner_app/admin.py
+++ travel_app/travel_planner_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from travel_planner_app.models import TravelGroup
+from travel_planner_app.models import TravelGroup, BudgetPlanner
 # Register your models here.
 admin.site.register(TravelGroup)
+admin.site.register(BudgetPlanner)
